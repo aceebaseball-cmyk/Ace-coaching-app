07a1db937a8894a19ab371a530b39c94818d3052
aceebaseball-cmyk <aceebaseball@gmail.com>
2026-04-26 11:33:00 -0500

    Create App component for ACE Coach Console

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>ACE Coach Console</h1>
+      <p>App is now running correctly.</p>
+    </div>
+  );
+}
